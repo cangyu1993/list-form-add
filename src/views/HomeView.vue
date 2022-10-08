6a8d783a36ec9d5aace4334c704127602572417f
cangyu1993 <1765244720@qq.com>
2022-10-08 12:08:58 +0800

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,100 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul class="list_area">
+      <li
+        v-for="(item, index) in list"
+        :key="index"
+        @click="editForm(index)"
+        class="li_list"
+      >
+        <p class="art_title">标题：{{ item.name }}</p>
+        <p class="art_title">内容：{{ item.content }}</p>
+      </li>
+      <li class="add_act" @click="addForm">+</li>
+    </ul>
+    <div class="form_area">
+      <input type="text" v-model="form.name" placeholder="name" />
+      <input type="text" v-model="form.content" placeholder="content" />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: "HomeView",
+  data() {
+    return {
+      list: [],
+      form: {
+        name: "",
+        content: "",
+        image: "",
+      },
+      index: 0,
+    };
+  },
+  watch: {
+    form: {
+      handler: function (newVal, oldVal) {
+        this.updataForm(newVal);
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    addForm() {
+      let addData = { name: "", content: "", image: "" };
+      this.list.push(addData);
+      this.form = addData;
+      if (this.list.length === 1) {
+        this.index = 0;
+      } else {
+        this.index = this.list.length - 1;
+      }
+    },
+    editForm(index) {
+      this.index = index;
+      let addData = this.list[index];
+      this.form = addData;
+    },
+    updataForm(data) {
+      this.list.splice(this.index, 1, data);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-around;
+  .list_area {
+    list-style: none;
+    height: 100vh;
+    width: 40%;
+    padding: 50px 0;
+    background-color: #ededed;
+    .li_list {
+      height: 50px;
+      background-color: #fff;
+    }
+    .li_list:nth-of-type(n + 2) {
+      margin-top: 20px;
+    }
+    .add_act {
+      width: 80%;
+      margin-left: 10%;
+      border: 1px dashed #ddd;
+      cursor: pointer;
+    }
+  }
+  .form_area {
+    height: 100vh;
+    width: 40%;
+    padding: 50px 0;
+    background-color: skyblue;
+    display: flex;
+    flex-direction: column;
   }
 }
-</script>
+</style>
